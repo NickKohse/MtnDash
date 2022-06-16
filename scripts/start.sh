bd24a31a7b0efd9451da415075ecca3983903942
Nick <nick.kohse@gmail.com>
2022-06-15 21:03:51 -0600

    bugfix

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 BASE_DIR="$(dirname "$0")"
-source "${BASE_DIR}/mtn/bin/activate"
-cd $BASE_DIR && gunicorn mtndash:server -b 0.0.0.0:8000
+source "${BASE_DIR}/../mtn/bin/activate"
+cd "${BASE_DIR}/.." && gunicorn mtndash:server -b 0.0.0.0:8000
